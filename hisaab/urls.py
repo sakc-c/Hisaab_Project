5394ee5c5cb133bbb2b8cdd13b634af3627da467
vanshikaj11 <vanshikaa2021@gmail.com>
2025-03-12 22:13:55 +0000

    conflict resolved

--- hisaab/urls.py
+++ hisaab/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
