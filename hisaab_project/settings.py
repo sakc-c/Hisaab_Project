10110203c72226a664ec96cfd9cc9d6bd1abaa5d
Sakshi Jashnani <sakshijashnani50@gmail.com>
2025-03-18 12:56:34 +0000

    adding domain name to allowed hosts

--- hisaab_project/settings.py
+++ hisaab_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["35.178.177.216", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["35.178.177.216", "localhost", "127.0.0.1", "hisaab.site"]
 
 
 # Application definition
